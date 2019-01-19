36666db1b1048f0821a600841bd946dbbab13420
Griffin Davis <griffincdavis@gmail.com>
2019-01-18 21:16:35 -0600

    Initial commit

---
+++ blog.py
@@ -0,0 +1,273 @@
+import webapp2
+import logging
+import re
+import jinja2
+import os
+import time
+import cgi
+import random
+import hashlib
+from google.appengine.ext import db
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
+
+class MyHandler(webapp2.RequestHandler):
+    def write(self, *writeArgs):
+        self.response.write(" : ".join(writeArgs))
+
+    def render_str(self, template, **params):
+        tplt = JINJA_ENVIRONMENT.get_template('templates/'+template)
+        return tplt.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+def hash_str(s):
+   hashed = hashlib.md5(str(s))
+   return hashed.hexdigest()
+
+def make_secure_val(s):
+   hashed = hash_str(s)
+   output = str(s) + "|" + str(hashed)
+   return output
+
+def check_secure_val(h):
+   barLoc = h.find('|')
+   s = h[:barLoc]
+   if (make_secure_val(s) == h):
+      return s
+   else:
+      return None
+
+def make_salt():
+    salt = ""
+    for i in range(25):
+        x = random.randint(33, 126)
+        while (x == 34 or x == 39 or x == 124):
+            x = random.randint(33, 126)
+        salt = salt + str(chr(x))
+    logging.info("SALT: " + salt)
+    return salt
+
+def make_pw_hash(name, pw, salt=None):
+    if salt is None:
+        salt = make_salt()
+    hash = hash_str(str(name) + str(pw) + str(salt))
+    output = str(hash) + "|" + str(salt)
+    return output
+
+def valid_pw(name, pw, h):
+    barLoc = h.find('|')
+    salt = h[barLoc:]
+    return (make_pw_hash(name, pw, salt) == h)
+
+USER_RE = re.compile(r'^[\w\-]{3,20}$')
+def valid_username(username):
+    return USER_RE.match(username)
+
+PASSWORD_RE = re.compile(r'^\S{3,20}$')
+def valid_password(username):
+    return PASSWORD_RE.match(username)
+
+EMAIL_RE = re.compile(r'^(\S+@\S+\.\S+)?$')
+def valid_email(username):
+    return EMAIL_RE.match(username)
+
+def escape_html(s):
+   return cgi.escape(s, quote = True)
+
+class PostDB(db.Model):
+    subject = db.StringProperty()
+    content = db.TextProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+
+class Users(db.Model):
+    username = db.StringProperty()
+    passwordHash = db.StringProperty()
+    email = db.StringProperty()
+
+class HomeRedirect(MyHandler):
+    def get(self):
+        self.redirect('/blog/welcome')
+
+class Signup(MyHandler):
+    def write_signup(self, username_error_msg="", password_error_msg="", verify_error_msg="", email_error_msg="", user_username="", user_email=""):
+        signupValues = {"error_username": username_error_msg,
+                                      "error_password": password_error_msg,
+                                      "error_verify"  : verify_error_msg,
+                                      "error_email"   : email_error_msg,
+                                      "username_value": escape_html(user_username),
+                                      "email_value"   : escape_html(user_email)}
+
+        signup = JINJA_ENVIRONMENT.get_template('templates/signup.html')
+        self.response.write(signup.render(signupValues))
+
+    def get(self):
+        logging.info("********** Signup GET **********")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.write_signup()
+
+    def post(self):
+        user_username = self.request.get('username')
+        user_password = self.request.get('password')
+        user_verify = self.request.get('verify')
+        user_email = self.request.get('email')
+
+        user_username_v = valid_username(user_username)
+        user_password_v = valid_password(user_password)
+        user_email_v = valid_email(user_email)
+        user_verify_v = True
+        user_exists_v = True
+
+        username_error_msg = password_error_msg = verify_error_msg = email_error_msg = ""
+
+        userInDB = db.GqlQuery("SELECT * FROM Users WHERE username = '%s'" % user_username).get()
+
+        if (userInDB is not None):
+            logging.info("USERINDB: " + userInDB.username)
+            user_exists_v = False
+            username_error_msg = "That user already exists."
+
+        if not(user_username_v):
+            username_error_msg = "That's not a valid username."
+
+        if (user_password != user_verify):
+            user_verify_v = False
+            password_error_msg = "Passwords do not match."
+        elif not(user_password_v):
+            password_error_msg = "That's not a valid password."
+        if (user_email != "") and not(user_email_v):
+            email_error_msg = "That's not a valid email."
+
+        if not(user_username_v and user_password_v and user_verify_v and ((user_email == "") or user_email_v) and (user_password == user_verify) and user_exists_v):
+            self.write_signup(username_error_msg, password_error_msg, verify_error_msg, email_error_msg, user_username, user_email)
+        else:
+            ##User signup is valid
+            userInst = Users()
+            userInst.username = user_username
+            userInst.passwordHash = make_pw_hash(user_username, user_password)
+            userInst.email = user_email
+            userInst.put()
+            time.sleep(.2)
+            userID = userInst.key().id()
+            userIDSecure = make_secure_val(userID)
+            self.response.headers.add_header('Set-Cookie', 'user_id=%s; Path=/' % userIDSecure)
+            self.redirect('/blog/welcome')
+
+class Login(MyHandler):
+    def write_login(self, login_error_msg=""):
+        loginValues = {"login_error": login_error_msg}
+        login = JINJA_ENVIRONMENT.get_template('templates/login.html')
+        self.response.write(login.render(loginValues))
+
+    def get(self):
+        logging.info("********** Login GET **********")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.write_login()
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        user = db.GqlQuery("SELECT * FROM Users WHERE username = '%s'" % username).get()
+
+        if (user is not None):
+            ##User exists
+            logging.info("HERE1")
+            hash = user.passwordHash
+            if (valid_pw(username, password, hash) is not None):
+                ##Username and password are valid
+                userID = user.key().id()
+                userIDSecure = make_secure_val(userID)
+                self.response.headers.add_header('Set-Cookie', 'user_id=%s; Path=/' % userIDSecure)
+                self.redirect('/blog/welcome')
+
+        login_error_msg = "Invalid login"
+        self.write_login(login_error_msg)
+
+class Logout(MyHandler):
+    def get(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+        self.redirect('/blog/login')
+
+class Welcome(MyHandler):
+    def write_welcome(self, username=""):
+        welcomeValues = {"username": username}
+        welcome = JINJA_ENVIRONMENT.get_template('templates/welcome.html')
+        self.response.write(welcome.render(welcomeValues))
+
+    def get(self):
+        logging.info("********** Welcome GET **********")
+        self.response.headers['Content-Type'] = 'text/html'
+
+        userIDHashed = self.request.cookies.get('user_id', '0')
+        userID = check_secure_val(userIDHashed)
+        if (userID == None):
+            self.redirect('/blog/login')
+        else:
+            user = Users.get_by_id(int(userID))
+            self.write_welcome(user.username)
+
+class Blog(MyHandler):
+    def renderPosts(self):
+        posts = db.GqlQuery("SELECT * FROM PostDB ORDER BY created DESC limit 10")
+        self.render("posts.html", posts=posts)
+
+    def get(self):
+        logging.info("********** Blog GET **********")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.renderPosts();
+
+class NewPost(MyHandler):
+    def renderNewPost(self, inputError=""):
+        pageValues = {"ph_error": inputError}
+        page = JINJA_ENVIRONMENT.get_template('templates/newpost.html')
+        self.response.write(page.render(pageValues))
+
+    def get(self):
+        logging.info("********** NewPost GET **********")
+        self.renderNewPost()
+
+    def post(self):
+        logging.info("********** NewPost POST **********")
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+
+        logging.info("SUBJECT: " + str(subject) + " CONTENT: " + str(content))
+
+        if ((not subject) or (not content)):
+            inputError = "Please provide both a subject and content"
+            self.renderNewPost(inputError)
+        else:
+            postInst = PostDB()
+            postInst.subject = subject
+            postInst.content = content
+            postInst.put()
+            time.sleep(.2)
+            postID = postInst.key().id()
+            self.redirect('/blog/' + str(postID))
+
+class PostHandler(MyHandler):
+    def renderPost(self, posts):
+        self.render("posts.html", posts=posts)
+
+    def get(self, postID):
+        logging.info("********** PostHandler GET **********")
+
+        post = PostDB.get_by_id(int(postID))
+        posts = [post]
+        self.renderPost(posts)
+
+
+application = webapp2.WSGIApplication([
+    ('/', HomeRedirect),
+    (r'/blog/signup/?', Signup),
+    (r'/blog/login/?', Login),
+    (r'/blog/logout/?', Logout),
+    (r'/blog/welcome/?', Welcome),
+    (r'/blog/?', Blog),
+    ('/blog/newpost/?', NewPost),
+    (r'/blog/(\d+)/?', PostHandler)
+], debug=True)
